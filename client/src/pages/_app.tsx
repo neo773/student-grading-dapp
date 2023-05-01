39d2a4d68cd50107141b476a628a2c3ba4aacb31
neo773 <neo773@protonmail.com>
2023-05-02 04:23:26 +0530

    cleanup app.tsx

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,32 +1,28 @@
 import { StoreProvider } from "easy-peasy"
 import { store } from "../store/store"
-import type { AppProps } from 'next/app'
-import { Toaster } from 'react-hot-toast';
-// import "react-csv-importer/dist/index.css"
-import '../index.css'
-import Header from "../components/Header/Header";
-// import { Html } from "next/document";
-import dynamic from "next/dynamic";
-import { useEffect } from "react";
-import { trpc } from "../utils/trpc";
+import type { AppProps } from "next/app"
+import { Toaster } from "react-hot-toast"
+import Header from "../components/Header/Header"
+import dynamic from "next/dynamic"
+import { trpc } from "../utils/trpc"
+import "../index.css"
+
 // @ts-ignore
 const NoSSRPage = dynamic(() => Promise.resolve(({ children }) => children), {
-    ssr: false,
-  });
-  
-  
-  
-  const App = ({ Component, pageProps, }: AppProps) => {
-    return (
-        // @ts-ignore
-        <NoSSRPage>
-        <StoreProvider store={store}>
-            <Header />
-            <Component {...pageProps} />
-            <Toaster />
-        </StoreProvider>
-        </NoSSRPage>
-    )
+  ssr: false,
+})
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    // @ts-ignore
+    <NoSSRPage>
+      <StoreProvider store={store}>
+        <Header />
+        <Component {...pageProps} />
+        <Toaster />
+      </StoreProvider>
+    </NoSSRPage>
+  )
 }
 
-export default trpc.withTRPC(App);
+export default trpc.withTRPC(App)
